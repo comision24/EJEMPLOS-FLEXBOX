7e8f8720bd7cbfa1169663e55e6d06375359f707
comision24 <comision24formar@gmail.com>
2024-01-09 21:25:31 -0300

    ejemplos terminados

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const port = 3030;
+const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/home.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/flexbox.html"));
+});
 
-app.listen(port,() => console.log(`http://localhost:${port}`));
-
+app.listen(port, () => console.log(`http://localhost:${port}`));
